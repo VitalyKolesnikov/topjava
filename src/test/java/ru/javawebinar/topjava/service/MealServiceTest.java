7add71c8327177c00445c6ac931bbbb6c7a06acf
VitalyKolesnikov <vitaly.kolesnikov88@gmail.com>
2020-06-24 23:56:27 +0300

    HW3 optional add duplicate meal test

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -5,13 +5,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDateTime;
 
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThrows;
@@ -86,5 +91,11 @@ public class MealServiceTest {
         newMeal.setId(newId);
         assertMatch(created, newMeal);
         assertMatch(service.get(newId, USER_ID), newMeal);
+    }
+
+    @Test
+    public void duplicateMealDatetimeCreate() throws Exception {
+        assertThrows(DataAccessException.class, () ->
+                service.create(new Meal(LocalDateTime.of(2020, 6, 19, 16, 45), "Duplicate Обед", 1100), USER_ID));
     }
 }
